c7990dd0d9e08e5ec245167dd28861dfb662836a
cdonal91 <89680346+cdonal91@users.noreply.github.com>
2021-11-11 21:29:57 +1100

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [searched, setSearched] = useState(null);
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searched}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSearched(event) {
