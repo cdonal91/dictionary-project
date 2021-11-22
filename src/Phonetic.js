04a7c82df8cdc15a79210a66ffbbc4c5132da7f8
cdonal91 <89680346+cdonal91@users.noreply.github.com>
2021-11-22 23:25:14 +1100

    3rd styling

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,10 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio}>Listen</a>
-      <br />
       {props.phonetic.text}
     </div>
   );
